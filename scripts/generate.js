0d375270e905152ace2c53f9abcb0e227784a443
Brad Rydzewski <bradley.rydzewski@harness.io>
2023-04-03 12:06:40 -0400

    add error when gofmt or goimports not available

--- scripts/generate.js
+++ scripts/generate.js
@@ -77,6 +77,20 @@ Object.entries(schema.definitions).forEach(([k, v]) => {
     console.log(`dist/go/${struct.path}`)
 });
 
-// format generated files and ensure imports
-exec("gofmt -s -w dist/go/*.go")
-exec("goimports -w dist/go/*.go")
+// format generated files
+exec("gofmt -s -w dist/go/*.go", (err) => {
+  if (err) {
+    console.log("ensure gofmt is installed and in your PATH")
+    console.error(err);
+    process.exit(1)
+  }
+});
+
+// import all dependencies if needed
+exec("goimports -w dist/go/*.go", (err) => {
+  if (err) {
+    console.log("ensure goimports is installed and in your PATH")
+    console.error(err);
+    process.exit(1)
+  }
+});
